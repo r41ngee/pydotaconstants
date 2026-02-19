e709a14f1106101c8405f720e67429ef55c1e92c
r41ngee <r41ngee@yandex.ru>
2026-02-19 12:10:15 +0300

    Update version to 0.2.0 and load abilities and locals data from JSON files

--- src/pydotaconstants/__init__.py
+++ src/pydotaconstants/__init__.py
@@ -3,3 +3,9 @@ from importlib import resources
 
 with resources.files("pydotaconstants.data").joinpath("heroes.json").open(encoding="utf-8") as f:
     HEROES = json.load(f)
+
+with resources.files("pydotaconstants.data").joinpath("abilities.json").open(encoding="utf-8") as f:
+    ABILITIES = json.load(f)
+
+with resources.files("pydotaconstants.data").joinpath("locals.json").open(encoding="utf-8") as f:
+    LOCALS = json.load(f)
