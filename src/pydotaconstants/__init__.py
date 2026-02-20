3ddb940a1e876e227dc0b8b56feee986a7757d6a
r41ngee <r41ngee@yandex.ru>
2026-02-21 02:04:46 +0300

    Refactor data loading: move HEROES, ABILITIES, and LOCALS loading to core.py

--- src/pydotaconstants/__init__.py
+++ src/pydotaconstants/__init__.py
@@ -1,11 +1,1 @@
-import json
-from importlib import resources
-
-with resources.files("pydotaconstants.data").joinpath("heroes.json").open(encoding="utf-8") as f:
-    HEROES = json.load(f)
-
-with resources.files("pydotaconstants.data").joinpath("abilities.json").open(encoding="utf-8") as f:
-    ABILITIES = json.load(f)
-
-with resources.files("pydotaconstants.data").joinpath("locals.json").open(encoding="utf-8") as f:
-    LOCALS = json.load(f)
+from .core import *
