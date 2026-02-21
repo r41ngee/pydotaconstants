c6a673888916d81a5173cb33a06391f8dfdb15f6
r41ngee <r41ngee@yandex.ru>
2026-02-21 06:04:03 +0300

    switch data loading from JSON to pickle format + bump stable

--- src/pydotaconstants/_core.py
+++ src/pydotaconstants/_core.py
@@ -1,11 +1,11 @@
-import json
+import pickle
 from importlib import resources
 
-with resources.files("pydotaconstants.data").joinpath("heroes.json").open(encoding="utf-8") as f:
-    HEROES = json.load(f)
+with resources.files("pydotaconstants.data").joinpath("heroes.pkl").open("rb") as f:
+    HEROES = pickle.load(f)
 
-with resources.files("pydotaconstants.data").joinpath("abilities.json").open(encoding="utf-8") as f:
-    ABILITIES = json.load(f)
+with resources.files("pydotaconstants.data").joinpath("abilities.pkl").open("rb") as f:
+    ABILITIES = pickle.load(f)
 
-with resources.files("pydotaconstants.data").joinpath("locals.json").open(encoding="utf-8") as f:
-    LOCALS = json.load(f)
+with resources.files("pydotaconstants.data").joinpath("locals.pkl").open("rb") as f:
+    LOCALS = pickle.load(f)
