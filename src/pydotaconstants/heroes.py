a16a1565dfc8cf52cc953ac295250c83c801e2aa
r41ngee <r41ngee@yandex.ru>
2026-02-21 07:19:04 +0300

    Add hero retrieval methods to Hero class
    Implemented getByName and getById methods for the Hero class to facilitate fetching Hero objects by codename and HeroID, respectively. Updated docstrings for clarity on return values and error handling.

--- src/pydotaconstants/heroes.py
+++ src/pydotaconstants/heroes.py
@@ -1,9 +1,8 @@
 from ._core import _HEROES, _LOCALS
-_heroes = _HEROES
-
 
 class Hero():
     """Represents single hero data
+    should not be created with constructor
     """
     def __init__(self, name: str, kv: dict):
         self._name = name
@@ -39,6 +38,39 @@ class Hero():
         example: Axe
 
         Returns:
-            str: hero name
+            str: hero name. Returns empty string if incorrect codename
         """
         return _LOCALS.get(self.name + ":n", "")
+
+    def getByName(name: str) -> Hero:
+        """Get Hero object from hero codename
+
+        Args:
+            name (str): hero codename
+
+        Returns:
+            Hero: Hero object
+        """        
+        return Hero(name, _HEROES[name])
+
+    def getById(id: int | str) -> Hero:
+        """Get Hero object by HeroID
+
+        Args:
+            id (int | str): HeroID
+
+        Raises:
+            IndexError: incorrect hero id
+
+
+        Returns:
+            Hero: Hero object
+        """
+        id = str(id)
+
+        for i in _HEROES:
+            hero_kv = _HEROES[i]
+            if hero_kv["HeroID"] == id:
+                return Hero(i, hero_kv)
+
+        raise IndexError(f"ID {id} not found.")
